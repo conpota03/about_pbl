92f63d46039f7a2e5688a502f7602d32c9d2628d
conpota03 <the.ts.tomo@gmail.com>
2023-01-14 11:59:43 +0900

    JSを変更して、数字を入れ替えて画像NOを変更できるように書き換えました

--- scripts/02about-pbl.js
+++ scripts/02about-pbl.js
@@ -1,24 +1,13 @@
-/* 画像の格納 */
-var images_src = new Array("images/pblPL01.png", "images/pblPL02.png", "images/pblPL03.png", "images/pblPL04.png", "images/pblPL05.png", "images/pblPL06.png", "images/pblPL07.png");
-var num = 0;
-/* 進む戻るで画像を変更 */
-document.getElementById("images").src = images_src[num];
+var imagesNo = 1;
 
 function go_forward() {
-    if (num == 6) {
-        num = 0;
-    } else {
-        num++;
-    }
-    document.getElementById("images").src = images_src[num];
+    imagesNo++;
+    document.getElementById("images").src = "images/pblPL0" + imagesNo + ".png";
+    document.getElementById("img-No").innerText = imagesNo + " / 7 ";
 }
 
 function go_back() {
-    if (num == 0) {
-        num = 2;
-    } else {
-        num--;
-    }
-    document.getElementById("images").src = images_src[num];
+    imagesNo--;
+    document.getElementById("images").src = "images/pblPL0" + imagesNo + ".png";
+    document.getElementById("img-No").innerText = imagesNo + " / 7 ";
 }
-/* 文字列の変更 */
